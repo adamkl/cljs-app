3856a02486a54e1c4265f27964f9a41ec1426fbb
Adam K-L <adamkl@users.noreply.github.com>
2021-04-23 23:41:46 -0400

    update cljs-express dep

--- src/cljs_app.cljs
+++ src/cljs_app.cljs
@@ -1,9 +1,15 @@
 (ns cljs-app
-  (:require [cljs-express :refer [express start]]))
+  (:require [cljs-express :refer [express start stop]]))
 
 (def routes [["/hello" :get (fn [ctx] (assoc ctx :response {:status 200
                                                             :body "Hello World"}))]])
-(def app (atom (express {:routes routes})))
+(def server (atom nil))
 
 (defn main []
-  (start @app 3000 #(print "server started")))
+  (reset! server (-> (express {:routes routes})
+                     (start  3000 #(print "server started")))))
+
+(comment
+  (stop @server)
+  (start @server 3000)
+  (comment))
